c8b5eb7ccf30896cf08f6a17dcd63a3783d04111
redinat <150556472+redinat@users.noreply.github.com>
2024-11-30 12:46:12 +0100

    testing push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Menu from './Components/Menu';
-import Catering from './Components/Catering';
-import Shop from './Components/Shop';
-import Contact from './Components/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import Catering from "./Components/Catering";
+import Shop from "./Components/Shop";
+import Contact from "./Components/Contact";
+//testing push
 
 function App() {
   return (
@@ -13,11 +14,11 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path ="/" element={<Home/>}></Route>
-          <Route path ="Menu" element ={<Menu/>}></Route>
-          <Route path ="Catering" element ={<Catering/>}></Route>
-          <Route path = "Shop" element={<Shop/>}></Route>
-          <Route path ="Contact" element={<Contact/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="Menu" element={<Menu />}></Route>
+          <Route path="Catering" element={<Catering />}></Route>
+          <Route path="Shop" element={<Shop />}></Route>
+          <Route path="Contact" element={<Contact />}></Route>
         </Routes>
       </Router>
     </>
